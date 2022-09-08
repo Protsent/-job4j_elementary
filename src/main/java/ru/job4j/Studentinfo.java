8f81eec530cd67ac4e7458bc8dd7927d94315fc0
prots <protsent@bk.ru>
2022-09-08 22:13:30 +0300

    3.3 Фиксация изменения на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Andrey Protsenko");
+        System.out.println("08.05.1976");
+    }
+}
